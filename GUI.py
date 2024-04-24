73f5c0786a3a31b026cf8daf739284bcc80c3a69
suchok94 <suchok94@gmail.com>
2024-04-24 22:14:39 +0300

    add func main_menu + func GUI. main_menu_input, check_exit, incorrect data, exit

--- GUI.py
+++ GUI.py
@@ -7,3 +7,40 @@ def main_menu_text():
     print("Load game")
     print("Exit")
 
+def main_menu_input():
+    main_menu_text()
+    print('Введите команду для продолжения...')
+    return input()
+
+def check_exit():
+    print('Вы уверены что хотите выйти?')
+    print('Y/N')
+    return input()
+
+
+def incorrect_data():
+    print('Вы ввели неправильные данные')
+
+
+def exit():
+    print('GoodBye!')
+
+# def input_player():
+
+
+
+
+
+    # data = input("Введите свой выбор: ")
+    # while data:
+    #     if data == "New game":
+    #         return 1
+    #     elif data == "Continue":
+    #         return 1
+    #     elif data == "Load game":
+    #         return 1
+    #     elif data == "Exit":
+    #         return 1
+    #     else:
+    #         print("Вы ввели неправильное значение: ")
+    #         return True
--- mainMenu.py
+++ mainMenu.py
@@ -3,6 +3,22 @@ import GUI
 def welcome():
     GUI.welcome()
     main_menu()
+    GUI.exit()
 
-def main_menu()
-    GUI.main_menu_text()
+def main_menu():
+    while True:
+        data = GUI.main_menu_input().lower()
+        if data == 'exit':
+            check_exit = GUI.check_exit().lower()
+            if check_exit == 'y' or check_exit == 'yes':
+                return
+            if check_exit == 'n' or check_exit == 'no':
+                continue
+            else:
+                GUI.incorrect_data()
+        if data == 'new game':
+            print('new game')
+        elif data == 'continue':
+            print('ncontinue')
+        elif data == 'load game':
+            print('load game')
